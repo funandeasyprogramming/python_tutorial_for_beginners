224bd964decbbb59f73b05005fde33c2d7116f50
gopokes1986 <funandeasyprogramming@gmail.com>
2020-08-20 19:24:50 -0700

    numbers

---
+++ numbers/numbers.py
@@ -0,0 +1,237 @@
+# Numbers - Integer, Float, Boolean, Complex
+
+# Integer - whole number either positive or negative without decimals.
+
+print (5 + 7)
+print (7-5)
+print (5-7)
+print (5 * 7)
+print (7/5)
+print (10/2)
+
+print (7 % 5)
+print (10 % 3)
+
+print (2 ** 3)
+print (3 ** 2)
+
+print (7 / 5)
+print (7 // 5)
+
+print (-7 // 5)
+
+print (3 + 5 * 2)
+print ((3+5) * 2)
+
+print (4+2/2*6**(2-1))
+
+12.0
+
+print (type (12))
+print (type(12.0))
+
+print (12.0 + 1.0)
+print (12.0 - 6.0)
+print (3.0 * 4.0)
+print (3.0 / 1.5)
+print (3.0 / 2.5)
+print (3.0 ** 2.0)
+print (3.0 // 1.5)
+print (3.0 // 1.7)
+
+print (4+2/2.0*6**(2.0-1))
+
+# complex - 4 + 2j
+
+first_complex = 4 + 2j
+second_complex = 2 + 1j
+
+# addition
+print (first_complex + second_complex)
+
+# subtraction
+print (first_complex - second_complex)
+
+first_complex = 4 + 2j
+second_complex = 2 + 1j
+# multiplication - foiling
+# (4+2j)(2+1j) -> 8 + 4j + 4j + 2j^2 -> 8 + 8j + 2j^2 -> j^ 2 = -1
+# -> 8 + 8j -2 -> 6 + 8j
+print (first_complex * second_complex)
+
+# division
+print (first_complex / second_complex)
+
+print (5 == 4)
+print (True == 1)
+print (False == 0)
+
+print ((5==5) == 1)
+
+float_example = 0.1 + 0.1 + 0.1
+print (float_example)
+print (0.3 == 0.3)
+
+# round, abs
+
+float_example = 1.54523
+print (round(float_example, 3))
+
+integer_example = -13
+float_example = -0.5
+
+print (abs(integer_example))
+print (abs(float_example))
+print (abs(first_complex))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
