333d2e5e7189fb8914479901ef412bcf7c0ac1de
gopokes1986 <funandeasyprogramming@gmail.com>
2020-09-19 11:53:53 -0700

    list complete

---
+++ list/list.py
@@ -0,0 +1,65 @@
+# list
+# 1. can hold various different data types
+# 2. order is maintained
+# 3. elements in the list are mutable
+
+# list_one = ["Danny", 12, 4.2, 12, [1, 2, "three"]]
+#
+# print (list_one[4][2])
+# print (list_one[:])
+# print (list_one[:1])
+# print (list_one[2:4])
+# print (list_one[2])
+# print (list_one[-1])
+# print (list_one[-4:-1])
+# print (list_one[1:5:2])
+
+# list_one = list()
+
+# list_one = ["Danny", 12, 4.2, 12, [1, 2, "three"]]
+#
+# list_one[7]
+#
+# list_one[4][2] = "four"
+# print (list_one)
+#
+# list_one[2] = 5.2
+# print (list_one)
+#
+# list_one[2:4] = [6.2, 15]
+# print (list_one)
+
+# list_one = ["Danny", 12, 4.2, 12, [1, 2, "three"]]
+#
+# # membership operator
+# print ("three" in list_one[4])
+#
+# # len
+# print (len(list_one[4]))
+
+list_one = ["Danny", 12, 4.2, 12, [1, 2, "three"]]
+# del
+# del list_one[2]
+# print (list_one)
+# del list_one[:4]
+# print (list_one)
+# del list_one[4][1]
+# print (list_one)
+list_one = ["Danny", 12, 4.2, 12, [1, 2, "three"]]
+list_two = [5, 6, 7]
+
+print (list_one + list_two)
+print (list_two * 3)
+
+
+
+
+
+
+
+
+
+
+
+
+
