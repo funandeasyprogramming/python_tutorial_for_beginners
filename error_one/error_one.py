6118c519757ba70f71c8467efe983cedd01dbc37
gopokes1986 <funandeasyprogramming@gmail.com>
2021-01-30 10:51:29 -0800

    error one complete

---
+++ error_one/error_one.py
@@ -0,0 +1,114 @@
+
+# syntax errors - using wrong Python's syntax
+# def test():
+
+# list_one = [1, 2, 3]
+# for i in list_one:
+#     print (i)
+
+# logic errors - caused by incorrect logic done by programmers.
+
+# expected output: 0, 2, 4, 6, 8, 10
+
+# for number in range(0, 11, 2):
+#     print (number)
+
+
+# expected output 2.5
+# result = (2 + 3) / 2
+#
+# print (result)
+
+
+# number_one = 3
+# number_two = 4
+#
+# if number_one + number_two == 7:
+#     print ("number is 7")
+# elif number_one + number_two < 7:
+#     print ("number is less than 7")
+#
+# if 6 < number_one + number_two < 10:
+#     print ("number is either 7, 8, 9")
+
+
+# Exceptions -
+# 1. Not syntax errors.
+# 2. Not logic errors.
+# 3. May occur during execution and it cannot complete the execution.
+
+# raise IndexError
+
+list_one = [1, 2, 3]
+# print (list_one[3])
+
+# for i in range(len(list_one)):
+#     print (list_one[i+1])
+
+# key error
+dict_one = {"a": 1, "b":2}
+# print (dict_one["c"])
+
+# value error
+
+# print (int("abcd"))
+
+# type error
+
+# print (list("abcd"))
+#
+# print (list(15))
+
+
+# print (dict([1, 2, 3]))
+
+#
+# variable_one = 1
+#
+# print (variable_two)
+
+# print (print_something())
+#
+# def print_something():
+#     print ("hi")
+
+# dict_one = {"a": 1, "b": 2}
+#
+# dict_one.update({"c": 3})
+# print (dict_one)
+#
+# list_one = [1, 2, 3]
+# list_one.update({"d": 4})
+# print (list_one)
+
+number = 10
+
+number.append("c")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
