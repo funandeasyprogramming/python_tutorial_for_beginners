19447e51f0cf33a896e39bc9540f4bb2af471001
gopokes1986 <funandeasyprogramming@gmail.com>
2021-02-13 09:52:20 -0800

    error three complete

---
+++ error_three/error_three.py
@@ -0,0 +1,89 @@
+# Below if from previous video
+def print_something(a_list):
+    try:
+        print("Hello {}".format(a_list[3]))
+    except IndexError as e:
+        print(repr(e))
+        try:
+            raise TypeError
+        except TypeError as e:
+            print(repr(e))
+    except AttributeError as e:
+        print(repr(e))
+    else:
+        print("There was no error in the try block")
+    finally:
+        print("This always gets run")
+
+#
+# print_something(["Danny", "Eddy", "Paul"])
+
+
+# Unit Conversion Program from previous videos
+
+
+def get_user_input():
+    print("MENU 1) Unit Conversion  2) Quit")
+    try:
+        menu = int(input("Choose an option from the MENU: "))
+
+        if menu == 1:
+            number = float(input("Enter a number: "))
+            from_unit = input("Enter the From Unit: ")
+            to_unit = input("Enter the To Unit: ")
+            return {"menu": menu, "number": number,
+                    "from_unit": from_unit, "to_unit": to_unit}
+        elif menu == 2:
+            return {"menu": menu}
+
+    except ValueError as e:
+        print ("MENU or number only takes integer.")
+        return get_user_input()
+
+
+
+standard_unit_mapper = {"M": 1, "KM": 0.001,
+                        "CM": 100, "MM": 1000,
+                        "FT": 3.280839895,
+                        "IN": 39.37007874}
+
+
+def convert_unit():
+    try:
+        while True:
+            user_input = get_user_input()
+
+            if not user_input:
+                print ("The menu only takes 1 or 2")
+                return convert_unit()
+
+            if user_input.get("menu") == 2:
+                break
+
+            # try:
+            #     if user_input.get("menu") == 2:
+            #         break
+            # except AttributeError as e:
+            #     print ("The menu only takes 1 or 2")
+            #     return convert_unit()
+            number = user_input.get("number")
+            from_unit = user_input.get("from_unit").upper()
+            to_unit = user_input.get("to_unit").upper()
+
+            if not (from_unit in standard_unit_mapper or
+                    to_unit in standard_unit_mapper):
+                print("We only allow following units: {}"
+                      .format(", ".join(standard_unit_mapper.keys())))
+
+            else:
+                converted_value = standard_unit_mapper[to_unit] * number / \
+                                  standard_unit_mapper[from_unit]
+                result = "{} {} is {} {}".format(number, from_unit, converted_value, to_unit)
+                # Standard unit mapper approach.
+
+                print(result)
+    except KeyboardInterrupt as e:
+        print ("\n Someone quit the program manually")
+
+
+convert_unit()
